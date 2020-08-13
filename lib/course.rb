afcaa8cfeca65154fdd009732f9e8a2289cd92e9
Kyle Stough <kstough00@gmail.com>
2020-08-13 21:41:15 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -15,13 +15,5 @@ class Course
   def self.reset_all
     @@all.clear
   end
-  
-  def make_courses
-  self.get_courses.each do |post|
-    course = Course.new
-    course.title = post.css("h2").text
-    course.schedule = post.css(".date").text
-    course.description = post.css("p").text
-  end
+ 
 end
-
